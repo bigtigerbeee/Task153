290534375eb8fb3604974b485ae39dfcdae65715
excelaiyt <excelaiyt12@gmail.com>
2023-10-28 18:09:16 +0300

    add commit

---
+++ src/Task153.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Task153 {
+
+    public static void main(String[] args) {
+        int[] array1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 7};
+
+        System.out.println(Arrays.toString(array1));
+    }
+
+    public static void printArray(int[] numbers) {
+        int[] array1;
+        array1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 7};
+
+
+    }
+}
+
+
